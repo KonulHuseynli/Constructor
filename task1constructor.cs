aaa3b7dd56cfd5902677d37cac9e0663f7290a8e
KonulHuseynli <konulrh@code.edu.az>
2022-05-24 00:44:43 +0400

    creatednewtask

---
+++ task1constructor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Konul", "Huseynli", 19);
+            {
+                person1.constructor();
+            }
+        }
+    }
+}
